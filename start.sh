78fb8a5aae9fc0f5921bdeeeb4332b492bb22e5b
Eduardo Otubo <eduardo.otubo@gmail.com>
2015-11-20 15:42:55 +0100

    2015-11-20_15-42-51

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 #java -Xms1G -Xmx1G -jar minecraft_server.1.8.8.jar nogui
 
-java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=7 -XX:+AggressiveOpts -Xms512M -Xmx1G -jar $1
+java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -Xms2G -Xmx4G -jar $1
 
